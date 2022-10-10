4ed4fd3266f28d4f257b292bcb1a0d0714887614
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-10-10 12:54:54 -0300

    Feat - adicionado o modulo para obersar as mudanças p/ responsividade

--- alphabirdz/src/app/app.module.ts
+++ alphabirdz/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MenuListComponent } from './components/menu-list/menu-list.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuListComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
