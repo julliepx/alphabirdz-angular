57407be1c447052830bbea3f3a0560861e733200
Jullie Paixão <jlpaixaof@gmail.com>
2022-10-28 17:21:17 -0300

    sight imports

--- alphabirdz/src/app/app.module.ts
+++ alphabirdz/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,7 +34,9 @@ import { SightComponent } from './components/sight/sight.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [BirdService, PostService, UserService],
   bootstrap: [AppComponent]
--- alphabirdz/src/app/components/sight/sight.component.ts
+++ alphabirdz/src/app/components/sight/sight.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Bird } from 'src/app/interfaces/bird';
+import { Post } from 'src/app/interfaces/post';
+import { BirdService } from 'src/app/services/bird.service';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-sight',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SightComponent implements OnInit {
 
-  constructor() { }
+  sightForm!: FormGroup;
+
+  posts: Post[] = [];
+  birds: Bird[] = [];
+
+  constructor(private postService: PostService, private birdService: BirdService) {
+    this.getAllPosts();
+    this.getAllBirds();
+  }
 
   ngOnInit(): void {
   }
 
+  getAllPosts(): Array<Post> { 
+    this.postService.getAllPosts().subscribe(data => {this.posts = data});
+    
+    return this.posts;
+  }
+
+  getAllBirds(): Array<Bird> {
+    this.birdService.getAllBirds().subscribe(data => {this.birds = data});
+
+    return this.birds;
+  }
+
+  onSubmit(): void {
+    
+  }
 }
