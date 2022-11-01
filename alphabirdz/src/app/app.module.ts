d7b2f1d982c3a4d3b20667284dfc99adbf846787
Ariel Quaresma <arielquaresma90@gmail.com>
2022-11-01 14:57:00 -0300

    Atualização de testes

--- alphabirdz/src/app/app.module.ts
+++ alphabirdz/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- alphabirdz/src/app/components/login/login.component.spec.ts
+++ alphabirdz/src/app/components/login/login.component.spec.ts
@@ -1,5 +1,9 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule, NgForm } from '@angular/forms';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { UserService } from 'src/app/services/user.service';
+import { RegisterComponent } from '../register/register.component';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,7 +12,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [ReactiveFormsModule, FormsModule],
+      declarations: [LoginComponent],
+      providers: [UserService, HttpClient, HttpHandler]
     })
     .compileComponents();
 
@@ -20,4 +26,11 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should redirect to registerPage when user click on "cadastre-se" button', () => {
+    const registerComponent = TestBed.createComponent(RegisterComponent);
+    const registerButton = fixture.debugElement.nativeElement.querySelector('button');
+    registerButton.click();
+    expect(registerComponent).toBeTruthy();
+  })
 });
