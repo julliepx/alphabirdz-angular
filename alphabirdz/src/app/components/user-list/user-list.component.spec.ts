9293e6fb75cbcd8f989da14dd6689665c5d19bb6
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-11-01 17:02:56 -0300

    Feat issue #22

--- alphabirdz/src/app/components/user-list/user-list.component.spec.ts
+++ alphabirdz/src/app/components/user-list/user-list.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 import { UserListComponent } from './user-list.component';
 
@@ -8,7 +13,9 @@ describe('UserListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      imports: [ FormsModule ],
+      declarations: [ UserListComponent ],
+      providers: [ UserService, HttpClient, HttpHandler ]
     })
     .compileComponents();
 
@@ -20,4 +27,27 @@ describe('UserListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('Redirect login', () => {
+  //   const fixture = TestBed.createComponent(UserListComponent);
+  //   const fixture2 = TestBed.createComponent(LoginComponent).nativeElement;
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement.children[0].children[0].children[0].children[0].children[1];
+  //   compiled.click();
+  //   fixture.detectChanges();
+
+  //   console.log(fixture2.innerHTML)
+  // });
+
+  // it('Redirect register', () => {
+  //   const fixture = TestBed.createComponent(UserListComponent);
+  //   const fixture2 = TestBed.createComponent(RegisterComponent).nativeElement;
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement.children[0].children[0].children[1];
+  //   compiled.click();
+  //   fixture.detectChanges();
+
+  //   console.log(fixture2.innerHTML)
+
+  // });
 });
