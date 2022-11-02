2596d6a191a1497a123fece045f0eedf7d27feed
Ariel Quaresma <arielquaresma90@gmail.com>
2022-11-01 21:53:27 -0300

    fix landing page conflicts

--- alphabirdz/src/app/components/landing-page/landing-page.component.ts
+++ alphabirdz/src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void { }
+  constructor(public breakpointObserver: BreakpointObserver) { }
+  mobile: boolean = true;
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe(['(max-width: 992px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.mobile = true;
+        } else {
+          this.mobile = false;
+        }
+      });
+  }
 
 }
