ef61329223ba16153100ce74650de0125d739c74
Jullie Paixão <jlpaixaof@gmail.com>
2022-11-01 22:31:03 -0300

    Alguns testes

--- alphabirdz/src/app/components/header/header.component.spec.ts
+++ alphabirdz/src/app/components/header/header.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FeedComponent } from '../feed/feed.component';
+import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { MenuListComponent } from '../menu-list/menu-list.component';
+import { UserListComponent } from '../user-list/user-list.component';
 
 import { HeaderComponent } from './header.component';
 
@@ -20,4 +24,38 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the landing page component', () => {
+    const landingPageCom = TestBed.createComponent(LandingPageComponent);
+    const landingPageComInstance = landingPageCom.nativeElement;
+    const logo = fixture.nativeElement.querySelector('#logo');
+
+    logo.click();
+    fixture.detectChanges();
+
+    expect(landingPageComInstance.innerHTML).toContain('fotografe'); 
+  });
+
+  it('should render the menu list component', () => {
+    const menuListCom = TestBed.createComponent(MenuListComponent);
+    const menuListComInstance = menuListCom.nativeElement;
+    const menuList = fixture.nativeElement.querySelector('#menuButton');
+
+    menuList.click();
+    fixture.detectChanges();
+
+    expect(menuListComInstance.innerHTML).toContain('Feed');
+  });
+  
+  it('should render the feed component', () => {
+    const userMenuCom = TestBed.createComponent(UserListComponent);
+    const userMenuComInstance = userMenuCom.nativeElement;
+    const userMenuList = fixture.nativeElement.querySelector('#userMenuButton');
+
+    userMenuList.click();
+    fixture.detectChanges();
+
+    expect(userMenuComInstance.innerHTML).toContain('Login');  
+  });
 });
+
--- alphabirdz/src/app/components/sight/sight.component.spec.ts
+++ alphabirdz/src/app/components/sight/sight.component.spec.ts
@@ -1,4 +1,10 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { Post } from 'src/app/interfaces/post';
+import { BirdService } from 'src/app/services/bird.service';
+import { PostService } from 'src/app/services/post.service';
+import { UserService } from 'src/app/services/user.service';
 
 import { SightComponent } from './sight.component';
 
@@ -6,9 +12,36 @@ describe('SightComponent', () => {
   let component: SightComponent;
   let fixture: ComponentFixture<SightComponent>;
 
+  let post: Post = ({ 
+    id: 1,
+    image: 'postImage', 
+    location: 'postLocation', 
+    date: 'postDate', 
+    user: { 
+      id: 1, 
+      username: 'joe', 
+      email: 'joe@joe.com', 
+      password: '123456', 
+      profilePhoto: 'userPhoto' 
+    }, 
+    bird: { 
+      id: 1, 
+      image: 'birdImage', 
+      englishName: 'birdEnglishName', 
+      latinName: 'birdLatinName', 
+      portugueseName: 'birdPortugueseName', 
+      dominantColor: 'birdDominantColor', 
+      gender: 'birdGender', 
+      habitat: 'birdHabitat', 
+      family: 'birdFamily', 
+      birdSize: 'birdSize' 
+    } 
+  })
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SightComponent ]
+      declarations: [ SightComponent],
+      providers: [PostService, BirdService, UserService, HttpClient, HttpHandler, FormBuilder]
     })
     .compileComponents();
 
@@ -17,7 +50,51 @@ describe('SightComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the form', () => {
+    expect(fixture.nativeElement.querySelector('h1').textContent).toContain('Cadastrar Avistamento');
+  });
+
+  /* it('should send the form with the right data', () => {
+    const form = fixture.nativeElement.querySelector('form');
+    const input = fixture.nativeElement.querySelector('input');
+    const button = fixture.nativeElement.querySelector('button');
+
+    input.value = { 
+      id: 1,
+      image: 'postImage', 
+      location: 'postLocation', 
+      date: 'postDate', 
+      user: { 
+        id: 1, 
+        username: 'joe', 
+        email: 'joe@joe.com', 
+        password: '123456', 
+        profilePhoto: 'userPhoto' 
+      }, 
+      bird: { 
+        id: 1, 
+        image: 'birdImage', 
+        englishName: 'birdEnglishName', 
+        latinName: 'birdLatinName', 
+        portugueseName: 'birdPortugueseName', 
+        dominantColor: 'birdDominantColor', 
+        gender: 'birdGender', 
+        habitat: 'birdHabitat', 
+        family: 'birdFamily', 
+        birdSize: 'birdSize' 
+      }
+    }
+
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    button.click();
+    fixture.detectChanges();
+
+    expect(input.value).toContain(post);
+  }); */
 });
